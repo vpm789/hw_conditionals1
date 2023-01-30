8e90201ff3dc56e7cee8290fa0302888c17f94a9
vpm789 <vpm789@gmail.com>
2023-01-30 20:23:02 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1(){
+        System.out.println("Задача №1");
         byte age = 21;
         if (age >= 18) {
             System.out.println("Если возраст человека равен "+age+", то он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен "+age+", то он не достиг совершеннолетия, " +
                     "нужно немного подождать");
+        }
+    }
+
+    public static void task2(){
+        System.out.println("");
+        System.out.println("Задача №2");
+        byte degree = 4;
+        if (degree < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
         }
     }
 }
